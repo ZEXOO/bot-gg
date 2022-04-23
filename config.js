2669881147692a312ca3e7cc5f2707b71f9b5c19
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-04-23 18:35:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['60189830350', '60149431385', '60189830350'] // Masukan nomot kalian
-global.mods = ['60189830350', '60149431385'] // Moderator
+global.owner = ['6289529291185', '6289529291185', '6289529291185'] // Masukan nomot kalian
+global.mods = ['629529291185', '6289529291185'] // Moderator
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
 global.APIs = { // API Prefix
   // name: 'https://website'
